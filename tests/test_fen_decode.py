a2af137706a75f1db2f6f2e113b7c1196aaf1c74
Chris Shucksmith <chris@shucksmith.co.uk>
2022-11-04 15:57:35 +0000

    remove verilog top level from cocotb tests

--- tests/test_fen_decode.py
+++ tests/test_fen_decode.py
@@ -27,8 +27,8 @@ class FENDriver(StreamDriver):
 async def test_fen_decode(dut):
     await cocotb.start(Clock(dut.clk, 1000).start())
 
-    fd = FENDriver(dut.clk, dut.valid, dut.data, dut.sop, dut.eop)
-    rcv = StreamReceiver(dut.clk, dut.pos_valid, dut.pos_data, dut.pos_sop, dut.pos_eop)
+    fd = FENDriver(dut.clk, dut.in_valid, dut.in_data, dut.in_sop, dut.in_eop)
+    rcv = StreamReceiver(dut.clk, dut.o_pos_valid, dut.o_pos_data, dut.o_pos_sop, dut.o_pos_eop)
     await Timer(5, units="ns")
     await RisingEdge(dut.clk)  # wait for falling edge/"negedge"
 
@@ -56,3 +56,39 @@ async def test_fen_decode(dut):
     for i in range(64):
         assert bs[i] == expected[i], f"at cell {i} expected {expected[i]}"
     await Timer(20, units="ns")
+
+
+@cocotb.test()
+async def test_fen_opening(dut):
+    await cocotb.start(Clock(dut.clk, 1000).start())
+    fd = FENDriver(dut.clk, dut.in_valid, dut.in_data, dut.in_sop, dut.in_eop)
+    rcv = StreamReceiver(dut.clk, dut.o_pos_valid, dut.o_pos_data, dut.o_pos_sop, dut.o_pos_eop)
+    await Timer(5, units="ns")
+    await RisingEdge(dut.clk)  # wait for falling edge/"negedge"
+
+    await fd.send("rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1")
+    bs = await rcv.recv()
+    assert dut.o_hmcount.value == 0, "halfmove is not 0"
+    assert dut.o_fmcount.value == 1, "fullmove is not 29"
+    assert dut.o_wtp.value == 1, "expected white to play"
+    assert dut.o_castle.value == 15, "castling rights not empty"
+    assert dut.o_ep.value == 0, "no empassant"
+    #  K Q R B N P
+    #  1 2 3 4 5 6   +0 black (lower case)
+    #  9 A B C D E   +8 white (upper case)
+    assert len(bs) == 64
+    expected = (
+        b""
+        + b"\x03\x05\x04\x02\x01\x04\x05\x03"
+        + b"\x06\x06\x06\x06\x06\x06\x06\x06"
+        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+        + b"\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E"
+        + b"\x0B\x0D\x0C\x0A\x09\x0C\x0D\x0B"
+    )
+    for i in range(64):
+        assert bs[i] == expected[i], f"at cell {i} expected {expected[i]}"
+    await Timer(20, units="ns")
+
