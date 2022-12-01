d556a9e366408540d75bae2bdedaab40bfc18afa
Chris Shucksmith <chris@shucksmith.co.uk>
2022-12-01 21:06:58 +0000

    build expected piece bus data from pychess board

--- tests/cocotb_fen_decode.py
+++ tests/cocotb_fen_decode.py
@@ -16,49 +16,37 @@ class FENDriver(StreamDriver):
     is an ascii digit length field, which could have more digits.
     Over a UART you could send a linebreak to indicate the end.
     """
-
     async def send(self, fenstr: str, **kwargs):
         # validates or fires exception
-        chess.Board(fenstr)
+        board = chess.Board(fenstr)
         bs = fenstr.encode()
         await super().send(bs, **kwargs)
+        return board
 
 
-@cocotb.test()
-async def test_fen_decode(dut):
-    await cocotb.start(Clock(dut.clk, 1000).start())
+def get_binary_board(board):
+    #  K Q R B N P
+    #  1 2 3 4 5 6   +0 black (lower case)
+    #  9 A B C D E   +8 white (upper case)
+    encoding = dict([(c,i) for i,c in enumerate(' kqrbnp  KQRBNP') if c != ' '])
+    print(encoding)
+    print(board)
+    # {'K': 1, 'Q': 2, 'R': 3, 'B': 4, 'N': 5, 'P': 6, 'k': 9, 'q': 10, 'r': 11, 'b': 12, 'n': 13, 'p': 14}
+    binary_board = bytearray(64)
+    for rank in range(8):
+        for file in range(8):
+            sq = chess.square(rank, file)
+            p = board.piece_at(sq)
+            if p is not None:
+                binary_board[rank + (7-file)*8] = encoding[p.symbol()]
+    return bytes(binary_board)
 
-    fd = FENDriver(dut.clk, dut.in_valid, dut.in_data, dut.in_sop, dut.in_eop)
-    rcv = StreamReceiver(
-        dut.clk, dut.o_pos_valid, dut.o_pos_data, dut.o_pos_sop, dut.o_pos_eop
-    )
-    await Timer(5, units="ns")
-    await RisingEdge(dut.clk)  # wait for falling edge/"negedge"
-
-    await fd.send("8/5k2/8/8/5q2/3B4/8/4K3 w - - 0 29")
-    bs = await rcv.recv()
-    assert dut.o_hmcount.value == 0, "halfmove is not 0"
-    assert dut.o_fmcount.value == 29, "fullmove is not 29"
-    assert dut.o_wtp.value == 1, "expected white to play"
-    assert dut.o_castle.value == 0, "castling rights not empty"
-    assert dut.o_ep.value == 0, "no empassant"
-    #  K Q R B N P   +0 black (lower case)
-    #  1 2 3 4 5 6   +8 white (upper case)
+def assert_board(bs, expected):
+    print(bs)
+    print(expected)
     assert len(bs) == 64
-    expected = (
-        b""
-        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
-        + b"\x00\x00\x00\x00\x00\x01\x00\x00"
-        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
-        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
-        + b"\x00\x00\x00\x00\x00\x02\x00\x00"
-        + b"\x00\x00\x00\x0c\x00\x00\x00\x00"
-        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
-        + b"\x00\x00\x00\x00\x09\x00\x00\x00"
-    )
     for i in range(64):
         assert bs[i] == expected[i], f"at cell {i} expected {expected[i]}"
-    await Timer(2000, units="ns")
 
 
 @cocotb.test()
@@ -71,7 +59,7 @@ async def test_fen_opening(dut):
     await Timer(5, units="ns")
     await RisingEdge(dut.clk)  # wait for falling edge/"negedge"
 
-    await fd.send(
+    board = await fd.send(
         "rnbqkbnr/pppppppp/8/8/8/8/PPPPPPPP/RNBQKBNR w KQkq - 0 1", idler=IdleToggler()
     )
     bs = await rcv.recv()
@@ -83,56 +71,42 @@ async def test_fen_opening(dut):
     #  K Q R B N P
     #  1 2 3 4 5 6   +0 black (lower case)
     #  9 A B C D E   +8 white (upper case)
-    assert len(bs) == 64
-    expected = (
-        b""
-        + b"\x03\x05\x04\x02\x01\x04\x05\x03"
-        + b"\x06\x06\x06\x06\x06\x06\x06\x06"
-        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
-        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
-        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
-        + b"\x00\x00\x00\x00\x00\x00\x00\x00"
-        + b"\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E"
-        + b"\x0B\x0D\x0C\x0A\x09\x0C\x0D\x0B"
+    assert_board(
+        bs,
+        (
+            b""
+            + b"\x03\x05\x04\x02\x01\x04\x05\x03"
+            + b"\x06\x06\x06\x06\x06\x06\x06\x06"
+            + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+            + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+            + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+            + b"\x00\x00\x00\x00\x00\x00\x00\x00"
+            + b"\x0E\x0E\x0E\x0E\x0E\x0E\x0E\x0E"
+            + b"\x0B\x0D\x0C\x0A\x09\x0C\x0D\x0B"
+        ),
     )
-    for i in range(64):
-        assert bs[i] == expected[i], f"at cell {i} expected {expected[i]}"
+    assert_board(bs, get_binary_board(board))
+
     await Timer(2000, units="ns")
 
+    board = await fd.send("8/5k2/8/8/5q2/3B4/8/4K3 w - - 0 29")
+    bs = await rcv.recv()
+    assert dut.o_hmcount.value == 0, "halfmove is not 0"
+    assert dut.o_fmcount.value == 29, "fullmove is not 29"
+    assert dut.o_wtp.value == 1, "expected white to play"
+    assert dut.o_castle.value == 0, "castling rights not empty"
+    assert dut.o_ep.value == 0, "no empassant"
+    assert_board(bs, get_binary_board(board))
 
-@cocotb.test()
-async def test_longest_fenstring(dut):
+    await Timer(2000, units="ns")
 
-    await cocotb.start(Clock(dut.clk, 1000).start())
-    fd = FENDriver(dut.clk, dut.in_valid, dut.in_data, dut.in_sop, dut.in_eop)
-    rcv = StreamReceiver(
-        dut.clk, dut.o_pos_valid, dut.o_pos_data, dut.o_pos_sop, dut.o_pos_eop
-    )
-    await Timer(5, units="ns")
-    await RisingEdge(dut.clk)  # wait for falling edge/"negedge"
-
+    # test_longest_fenstring(dut):
     # 32 pieces, 64 squares, alternate
     # worst case fill of the internal FIFO
-    await fd.send(
+    board = await fd.send(
         "r1n1b1q1/k1b1n1r1/p1p1p1p1/p1p1p1p1/P1P1P1P1/P1P1P1P1/R1N1B1Q1/K1B1N1R1 w KQkq - 0 1",
         idler=IdleToggler(),
     )
     bs = await rcv.recv()
-    #  K Q R B N P
-    #  1 2 3 4 5 6   +0 black (lower case)
-    #  9 A B C D E   +8 white (upper case)
-    assert len(bs) == 64
-    expected = (
-        b""
-        + b"\x03\x00\x05\x00\x04\x00\x02\x00"
-        + b"\x01\x00\x04\x00\x05\x00\x03\x00"
-        + b"\x06\x00\x06\x00\x06\x00\x06\x00"
-        + b"\x06\x00\x06\x00\x06\x00\x06\x00"
-        + b"\x0E\x00\x0E\x00\x0E\x00\x0E\x00"
-        + b"\x0E\x00\x0E\x00\x0E\x00\x0E\x00"
-        + b"\x0B\x00\x0D\x00\x0C\x00\x0A\x00"
-        + b"\x09\x00\x0C\x00\x0D\x00\x0B\x00"
-    )
-    for i in range(64):
-        assert bs[i] == expected[i], f"at cell {i} expected {expected[i]}"
+    assert_board(bs, get_binary_board(board))
     await Timer(2000, units="ns")
--- tests/test_python_chess.py
+++ tests/test_python_chess.py
@@ -1,43 +1,34 @@
 import chess
 
 # https://en.wikibooks.org/wiki/Chess/Notating_The_Game
-board = chess.Board()
-print(board.legal_moves)
 
-board.push_san("e4")
-board.push_san("e5")
-board.push_san("Qh5")
-board.push_san("Nc6")
-print(board)
-print(board.legal_moves)
-board.push_san("Bc4")
-board.push_san("Nf6")
-print(board)
-print(board.legal_moves)
-board.push_san("Qxf7")
 
-print(board.is_checkmate())
-print(board.legal_moves)
-print(board)
-print(repr(board))
+def test_initial_moves():
+    board = chess.Board()
+    print(board.legal_moves)
+    assert len(list(board.legal_moves)) == 20
+    assert len(list(board.pseudo_legal_moves)) == 20
+    # print(board.is_checkmate())
 
 
 def test_kmoves_check():
-    # https://en.wikibooks.org/wiki/Chess/Notating_The_Game
     b = chess.Board("8/5k2/8/8/5q2/3B4/8/4K3 w - - 0 29")
     print(b)
     print(b.legal_moves)
     print(b.pseudo_legal_moves)
 
+    # <LegalMoveGenerator at 0x7f832c861a20 (Bh7, Bg6+, Ba6, Bf5, Bb5, Be4, Bc4+, Be2, Bc2, Bf1, Bb1, Ke2, Kd1)>
+    # <PseudoLegalMoveGenerator at 0x7f832c861a20 (Bh7, Bg6+, Ba6, Bf5, Bb5, Be4, Bc4+, Be2, Bc2, Bf1, Bb1, e1f2, Ke2, e1d2, e1f1, Kd1)>
+
     assert b.parse_san("Kd1") in b.legal_moves
     assert b.parse_san("Ke2") in b.legal_moves
     assert len(list(b.legal_moves)) == 13  # 2 for the king, 11 for B
-
-    print(b.pseudo_legal_moves)
     assert len(list(b.pseudo_legal_moves)) == 16  # 5 for the king, 11 for B
-    # parse_san parsed only valid moves
-    # assert b.parse_san('e1f2') in b.pseudo_legal_moves
 
+    # e1f2, e1d2, e1f1 leave K in check, not legal moves
+    # b.parse_san parses only legal moves
+    assert(chess.Move.from_uci("e1f2") in b.pseudo_legal_moves)
+    assert(chess.Move.from_uci("e1f2") not in b.legal_moves)
 
 def test_kmoves_promote():
     b = chess.Board("8/2P2P1k/8/8/8/8/8/4K3 w - - 0 29")
